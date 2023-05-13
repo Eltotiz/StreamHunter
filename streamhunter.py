546bfa468f38d3b18c927cbb67edb19db124957e
NoahOnFyre <noahonfyre@gmail.com>
2023-05-13 18:07:23 +0200

    Added quitting feature.

--- streamhunter.py
+++ streamhunter.py
@@ -30,22 +30,27 @@ print("""
                 Created by github.com/Eltotiz
                 """)
 input(" [+] Press enter to start!")
-print()
 print(" [+] Searching videos...")
-print()
 print()
 
 while True:
-    text = "".join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
-    minus = text.lower()
-    time.sleep(1)
-    src = requests.get(f"https://streamable.com/{minus}")
     try:
-        if src.status_code != 404:
-            print(Fore.GREEN + f" [+] https://streamable.com/{minus} VALID LINK")
-                
-    except Exception as e:
-        print()
-        print(" [?] There was an error connecting to streamable.com...")
-        print(f" [?] Error message: {e}")
-        input(" [?] Press enter to quit.")
+        text = "".join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+        minus = text.lower()
+        time.sleep(1)
+        src = requests.get(f"https://streamable.com/{minus}")
+        try:
+            if src.status_code != 404:
+                print(Fore.GREEN + f" [+] https://streamable.com/{minus} VALID LINK")
+
+        except Exception as e:
+            print()
+            print(" [?] There was an error connecting to streamable.com...")
+            print(f" [?] Error message: {e}")
+            input(" [?] Press enter to quit.")
+        exit(0)
+    except KeyboardInterrupt:
+        print(" [+] Quitting program...")
+        input(" [+] Press enter to quit.")
+        exit(0)
+        break
